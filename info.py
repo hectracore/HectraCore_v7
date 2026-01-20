1cd4e7f33148671109f95630bf1972fcb4ab79ae
HectraCore <hectracore@gmail.com>
2026-01-20 15:44:25 +0400

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ auth_channels     = environ.get("AUTH_CHANNELS", "-100")# Channels for force sub
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', 'https://files.catbox.moe/up9on1.png')    # QR code image for payments
+QR_CODE = environ.get('QR_CODE', 'https://graph.org/muzzzzz-01-20')    # QR code image for payments
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
